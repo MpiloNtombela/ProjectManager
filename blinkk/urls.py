ceafc697ac31fc250a603547ffebf3d392895fe2
mpilo ntombela <m.ntombela458@gmail.com>
2021-01-16 15:19:32 +0200

    resend email url added

--- blinkk/urls.py
+++ blinkk/urls.py
@@ -1,21 +1,24 @@
-"""blinkk URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from dj_rest_auth.registration.views import VerifyEmailView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+
+from frontend.views import index
+from users.api import ResendEmailConfirmationAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/password/reset/confirm/<str:uidb64>/<str:token>', index, name='password_reset_confirm'),
+
+    # endpoint to send key using post request
+    path('api/auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
+    re_path(r'^api/auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', index,
+            name='account_confirm_email'),
+    path('api/auth/resend-account-confirmation-email/', ResendEmailConfirmationAPI.as_view(),
+         name='resend-account-confirmation-email'),
+
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/auth/', include('dj_rest_auth.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('api/u/', include('users.urls')),
+    path('', include("frontend.urls")),
 ]
