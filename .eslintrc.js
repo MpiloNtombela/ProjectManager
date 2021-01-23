c5ff6b4f0795747f71dfe0bcc56b85d325f63494
mpilo ntombela <m.ntombela@gmail.com>
2021-01-23 05:05:17 +0200

    added esnlint config file

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks"],
+  rules: {
+    "react/no-danger": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "import/named": 2,
+    "import/no-unresolved": [2],
+  },
+  settings: {
+    "import/resolver": "webpack",
+  },
+};
