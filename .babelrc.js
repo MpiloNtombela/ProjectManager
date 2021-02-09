0f9640d73fcd8468cf330aa811575a6bb53ee5f3
mpilo ntombela <m.ntombela@gmail.com>
2021-02-09 18:49:59 +0200

    added new plugin to allow async writting async code

--- .babelrc.js
+++ .babelrc.js
@@ -1,21 +1,30 @@
-const presets = ["@babel/preset-env", "@babel/preset-react"]
+const presets = [
+  [
+    "@babel/preset-env",
+    {
+      "targets": {
+        "node": "14"
+      }
+    }
+  ], "@babel/preset-react"]
 
 const plugins = [
-    [
-        "babel-plugin-transform-imports",
-        {
-            "@material-ui/core" : {
-                // Use "transform: '@material-ui/core/${member}'," if your bundler does not support ES modules
-                "transform"        : "@material-ui/core/esm/${member}",
-                "preventFullImport": true
-            },
-            "@material-ui/icons": {
-                // Use "transform: '@material-ui/icons/${member}'," if your bundler does not support ES modules
-                "transform"        : "@material-ui/icons/esm/${member}",
-                "preventFullImport": true
-            }
-        }
-    ]
+  "@babel/plugin-transform-async-to-generator",
+  [
+    "babel-plugin-transform-imports",
+    {
+      "@material-ui/core": {
+        // Use "transform: '@material-ui/core/${member}'," if your bundler does not support ES modules
+        "transform": "@material-ui/core/esm/${member}",
+        "preventFullImport": true
+      },
+      "@material-ui/icons": {
+        // Use "transform: '@material-ui/icons/${member}'," if your bundler does not support ES modules
+        "transform": "@material-ui/icons/esm/${member}",
+        "preventFullImport": true
+      }
+    }
+  ]
 ];
 
 module.exports = {presets, plugins};
