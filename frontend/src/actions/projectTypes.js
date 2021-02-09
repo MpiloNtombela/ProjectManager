e0d956f63bd0f5da9efcd78b3773e920be679094
mpilo ntombela <m.ntombela@gmail.com>
2021-02-09 18:31:18 +0200

    added action creator and reducer to add and delete boards

---
+++ frontend/src/actions/projectTypes.js
@@ -0,0 +1,24 @@
+
+// request sent
+export const GET_USER_PROJECTS = 'GET_USER_PROJECTS'
+export const GET_PROJECT = 'GET_PROJECT'
+export const GET_BOARDS = 'GET_BOARDS'
+export const CREATE_BOARD = 'CREATE_BOARD';
+export const GET_TASK = 'GET_TASK'
+
+// request success
+export const PROJECTS_LOADED = 'PROJECTS_LOADED'
+export const PROJECT_LOADED = 'PROJECT_LOADED'
+export const BOARDS_LOADED = 'BOARDS_LOADED'
+export const TASK_LOADED = 'TASK_LOADED'
+export const BOARD_CREATED = 'BOARD_CREATED';
+export const BOARD_DELETED = 'BOARD_DELETED';
+
+// request failed
+export const PROJECTS_REQUEST_FAILED = 'PROJECTS_REQUEST_FAILED'
+export const PROJECT_REQUEST_FAILED = 'PROJECT_REQUEST_FAILED'
+export const BOARDS_REQUEST_FAILED = 'BOARDS_REQUEST_FAILED'
+export const TASK_REQUEST_FAILED = 'TASK_REQUEST_FAILED'
+
+// clear
+export const CLEAR_PROJECT_STATE = 'CLEAR_PROJECT_STATE';
