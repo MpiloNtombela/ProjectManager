cdbf7c74f099ad0c9be99089d645bc86bf60887d
mpilo ntombela <m.ntombela@gmail.com>
2021-02-09 18:32:00 +0200

    added action creator and reducer to add and delete boards

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,12 @@
-import { combineReducers } from "redux";
+import {combineReducers} from "redux";
 import auth from "./auth";
 import alerts from "./snackAlerts";
 import errors from "./errors";
+import projects from "./projects";
 
 export default combineReducers({
   alerts,
   auth,
   errors,
+  projectState: projects
 });
