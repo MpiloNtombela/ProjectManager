d985cdb5d9f738d0a17bf589bf8e6c6d8e2b3031
mpilo ntombela <m.ntombela@gmail.com>
2021-02-10 08:30:08 +0200

    created add task action creator and added it to reducers

--- frontend/src/actions/projects.js
+++ frontend/src/actions/projects.js
@@ -72,6 +72,34 @@ export const getTask = (id) => (dispatch, getState) => {
       dispatch({type: action.TASK_REQUEST_FAILED})
     });
 }
+/**
+ * @description creates an new task
+ * @param {string} name name of the task
+ * @param {string} id of the board for the task
+ * @param {number} idx index of the board in the store/state
+ * */
+
+export const addTask = (name, id, idx) => (dispatch, getState) => {
+  dispatch({type: action.ADD_TASK})
+  const body = JSON.stringify({name, project: id})
+  axios.post(`/api/projects/req/board/${id}/tasks/`, body, tokenConfig(getState().auth.token))
+    .then(res => {
+      dispatch({
+        type: action.TASK_ADDED,
+        payload: {
+          data: res.data,
+          index: idx
+        }
+      })
+    }).catch((err) => {
+    if (err.response) {
+      dispatch(createSnackAlert(err.response.data, err.response.status))
+    } else {
+      dispatch(createSnackAlert(err.message, 400))
+    }
+    dispatch({type: action.TASK_REQUEST_FAILED})
+  })
+}
 
 export const createBoard = (name, id) => (dispatch, getState) => {
   dispatch({type: action.CREATE_BOARD})
