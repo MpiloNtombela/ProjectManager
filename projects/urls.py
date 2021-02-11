0771edacac50682d2a7e5a59048cb64e65583210
mpilo ntombela <m.ntombela@gmail.com>
2021-02-11 22:56:15 +0200

    added the backend functionality to delete tasks

--- projects/urls.py
+++ projects/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from projects.api import (ProjectRetrieveAPI, ProjectBoardsListCreateAPI, TaskDetailsAPI, BoardDestroyAPI,
+from projects.api import (ProjectRetrieveAPI, ProjectBoardsListCreateAPI, TaskRetrieveDestroyAPI, BoardDestroyAPI,
                           ProjectTaskListCreateAPI, BoardTaskCreateAPI)
 
 urlpatterns = [
     path('req/project/<str:pk>/', ProjectRetrieveAPI.as_view(), name='project-retrieve'),
     path('req/project/<str:pk>/boards/', ProjectBoardsListCreateAPI.as_view(), name='project-boards'),
     path('req/project/<str:pk>/tasks/', ProjectTaskListCreateAPI.as_view(), name='project-tasks'),
-    path('req/board/<str:pk>/', BoardDestroyAPI.as_view(), name='board-details'),
+    path('req/board/<str:pk>/', BoardDestroyAPI.as_view(), name='board-destroy'),
     path('req/board/<str:pk>/tasks/', BoardTaskCreateAPI.as_view(), name='board-tasks'),
-    path('req/task/<str:pk>/', TaskDetailsAPI.as_view(), name='task-details'),
+    path('req/task/<str:pk>/', TaskRetrieveDestroyAPI.as_view(), name='task-retrieve-destroy'),
 ]
