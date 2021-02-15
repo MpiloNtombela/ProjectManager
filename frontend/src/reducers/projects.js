269d8e22fe419798d6f0aa96b0b87756f4884e3b
mpilo ntombela <m.ntombela458@gmail.com>
2021-02-15 05:27:41 +0200

    updated reducers to handle create and list of comments for the task

--- frontend/src/reducers/projects.js
+++ frontend/src/reducers/projects.js
@@ -13,7 +13,8 @@ const initialState = {
     task: null,
     tasks: null,
     isTaskLoading: false,
-    isAdding: false
+    isAdding: false,
+    isRequesting: false
   }
 };
 
@@ -35,6 +36,9 @@ export default function (state = initialState, {type, payload}) {
         break
       case action.CREATE_BOARD:
         draft.boardsState.isCreating = true
+        break
+      case action.TASK_VIEW_REQUESTING:
+        draft.tasksState.isRequesting = true
         break
 
       // request success
@@ -69,6 +73,10 @@ export default function (state = initialState, {type, payload}) {
       case action.BOARD_DELETED:
         draft.boardsState.boards.splice(payload, 1)
         break
+      case action.COMMENT_ADDED:
+        draft.tasksState.task['task_comments'].unshift(payload)
+        draft.tasksState.isRequesting = false
+        break
 
       // request failed
       case action.PROJECTS_REQUEST_FAILED:
@@ -80,9 +88,12 @@ export default function (state = initialState, {type, payload}) {
         draft.boardsState.isCreating = false
         break
       case action.TASK_REQUEST_FAILED:
+      case action.COMMENT_REQUEST_FAILED:
         draft.tasksState.isTaskLoading = false
         draft.tasksState.isAdding = false
+        draft.tasksState.isRequesting = false
         break
+
       // clear
       case action.CLEAR_PROJECT_STATE:
         return initialState
