653e501e70c259d4cb83f14954a097c80d18975a
mpilo ntombela <m.ntombela458@gmail.com>
2021-02-15 05:32:02 +0200

    added new button style(shadowed)

--- frontend/src/components/reuse/ReButtons.js
+++ frontend/src/components/reuse/ReButtons.js
@@ -1,4 +1,4 @@
-import React, {memo} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
@@ -19,63 +19,80 @@ const useStyles = makeStyles(theme => ({
       width: '2rem'
     }
   },
-  ButtonContainer: {
-    '& .cancelButton': {
-      background: theme.palette.error.main,
-      margin: '.25rem',
-      color: 'inherit'
+  primary: {
+    color: theme.palette.primary.main,
+    '&:focus': {
+      background: theme.palette.background.paper,
+      boxShadow: `0 0 4px 1px ${theme.palette.secondary.main}`
+    }
+  },
+  secondary: {
+    color: theme.palette.secondary.main,
+    '&:focus': {
+      boxShadow: `0 0 4px 1px ${theme.palette.secondary.main}`
+    }
+  },
+  error: {
+    margin: '.25rem',
+    color: theme.palette.error.main,
+    boxShadow: `0 0 5px 1px ${theme.palette.error.main}`
+  },
+  glowHover: {
+    background: theme.palette.background.paper,
+    boxShadow: `0 0 5px 1px rgba(0, 0, 0, .3)`,
+    '&:hover': {
+      background: theme.palette.background.paper,
+      color: 'inherit',
+      boxShadow: '0 0 5px 2px initial'
     },
-    '& .saveButton': {
-      background: theme.palette.primary.main,
-      margin: '.25rem',
-      color: 'inherit'
+    '&:focus': {
+      background: theme.palette.background.paper
     }
   }
 }))
 // eslint-disable-next-line react/display-name
-const FormSubmitButton = memo(({children, disabled, size}) => (
+const FormSubmitButton = React.memo(({children, disabled, size}) => (
   <Box sx={{my: 2}}>
-    <Button
-      disableElevation
-      type="submit"
-      fullWidth
-      color="primary"
-      variant="contained"
-      disabled={disabled}
-      size={size}>
+    <Button color="primary"
+            disableElevation
+            type="submit"
+            fullWidth
+            variant="contained"
+            disabled={disabled}
+            size={size}>
       {children}
     </Button>
   </Box>
 ));
 
+FormSubmitButton.propTypes = {
+  children: PropTypes.any,
+  disabled: PropTypes.bool,
+  size: PropTypes.string
+};
+
 const SaveCancelButtons = ({containerClass, saveButtonType, cancelButtonType, onClickSave, onClickCancel}) => {
   const classes = useStyles()
   return (
-    <Box sx={{mt: 1}} className={`${containerClass ? containerClass : classes.flexButtons} ${classes.ButtonContainer}`}>
-      <Avatar
-        component={ButtonBase}
-        type={cancelButtonType}
-        onClick={onClickCancel}
-        className={'cancelButton'}
-        aria-label='cancel button'>
+    <Box sx={{mt: 1}} className={`${containerClass ? containerClass : classes.flexButtons}`}>
+      <Avatar component={ButtonBase}
+              type={cancelButtonType}
+              onClick={onClickCancel}
+              className={`${classes.error} ${classes.glowHover}`}
+              aria-label='cancel button'>
         <Close/>
       </Avatar>
-      <Avatar
-        component={ButtonBase}
-        type={saveButtonType}
-        onClick={onClickSave}
-        className={'saveButton'}
-        aria-label="save button">
+      <Avatar component={ButtonBase}
+              type={saveButtonType}
+              onClick={onClickSave}
+              className={`${classes.primary} ${classes.glowHover}`}
+              aria-label="save button">
         <Done/>
       </Avatar>
     </Box>
   )
 }
-FormSubmitButton.propTypes = {
-  children: PropTypes.any,
-  disabled: PropTypes.bool,
-  size: PropTypes.string,
-};
+
 SaveCancelButtons.propTypes = {
   containerClass: PropTypes.any,
   saveButtonType: PropTypes.string,
@@ -84,5 +101,38 @@ SaveCancelButtons.propTypes = {
   onClickCancel: PropTypes.func
 };
 
-export default memo(SaveCancelButtons)
+const GlowButton = ({color, size, type, disabled, fullWidth, disableElevation, onClick, startIcon, endIcon, children}) => {
+  const classes = useStyles()
+  return (
+    <Button size={size}
+            color={'secondary'}
+            disableElevation={disableElevation}
+            type={type}
+            fullWidth={fullWidth}
+            variant="contained"
+            disabled={disabled}
+            onClick={onClick}
+            startIcon={startIcon}
+            endIcon={endIcon}
+            className={`${classes[color]} ${classes.glowHover}`}>
+      {children}
+    </Button>
+  )
+}
+
+GlowButton.propTypes = {
+  color: PropTypes.oneOf(['primary', 'secondary', 'error']).isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  type: PropTypes.oneOf(['submit', 'button', 'reset']),
+  disabled: PropTypes.bool,
+  fullWidth: PropTypes.bool,
+  disableElevation: PropTypes.bool,
+  onClick: PropTypes.func,
+  startIcon: PropTypes.element,
+  endIcon: PropTypes.element,
+  children: PropTypes.any
+}
+
+export default React.memo(SaveCancelButtons)
 export {FormSubmitButton}
+export {GlowButton}
