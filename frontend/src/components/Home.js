de1a43cd8be0517c685aaf624f2a3f37e6ba83f4
mpilo ntombela <m.ntombela458@gmail.com>
2021-02-16 21:50:13 +0200

    renamed ProjectPage to ProjectView for consistency

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,14 +2,13 @@ import React from 'react';
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
 
 const Home = () => {
     return (
         <Box component="main" sx={{mt: 2}}>
             <Container maxWidth="sm">
-                <Typography variant="subtitle1" component="h1">All projects</Typography>
-                <Divider style={{marginBottom: 8}}/>
+                <Typography variant="h5" component="h2">Your projects</Typography>
+                {/*<Divider style={{marginBottom: 8}}/>*/}
                 {/*{lists.map(list => <Box sx={{mb: 2}} key={list}><ProjectHome/></Box>)}*/}
             </Container>
         </Box>
