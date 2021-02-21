e0d12abdf109e06bbaeb5be2f85d28569eece509
mpilo ntombela <m.ntombela458@gmail.com>
2021-02-21 17:08:07 +0200

    added action creators to handle adding nad removing of users from task(members)

--- frontend/src/actions/projects/tasks.js
+++ frontend/src/actions/projects/tasks.js
@@ -60,7 +60,7 @@ export const addTask = (name, id, idx) => (dispatch, getState) => {
  * @param {string} id of the task to be deleted
  * @param {number} boardIndex index of the board the task is in at
  * @param {number} taskIndex index of the task in the board
-* */
+ * */
 export const deleteTask = (id, boardIndex, taskIndex) => (dispatch, getState) => {
   dispatch({type: action.TASK_VIEW_REQUESTING})
   axios.delete(`/api/projects/req/task/${id}/`, tokenConfig(getState().auth.token))
@@ -83,12 +83,44 @@ export const deleteTask = (id, boardIndex, taskIndex) => (dispatch, getState) =>
   })
 }
 
+/**
+ * @description add user to task members
+ * @param {string} taskId task id
+ * @param {string} userId user id
+ * @param {string} type alteration type [add | remove]
+ * */
+export const addRemoveMember = (taskId, userId, type) => (dispatch, getState) => {
+  if (type.toLowerCase() !== 'add' && type.toLowerCase() !== 'remove') {
+    return
+  }
+  dispatch({type: action.TASK_VIEW_REQUESTING})
+  const body = JSON.stringify({id: userId, type})
+  axios.patch(`/api/projects/req/task/${taskId}/members/`, body, tokenConfig(getState().auth.token))
+    .then(res => {
+      dispatch({
+        type: action.TASK_MEMBER_ALTERED,
+        payload: {
+          type,
+          data: res.data.response
+        }
+      })
+      dispatch(createSnackAlert(res.data.message, res.status))
+    }).catch((err) => {
+    if (err.response) {
+      dispatch(createSnackAlert(err.response.data, err.response.status))
+    } else {
+      dispatch(createSnackAlert(err.message, 400))
+    }
+    dispatch({type: action.TASK_REQUEST_FAILED})
+  })
+}
+
 
 /**
-* @description add a comment
+ * @description add a comment
  * @param {string} comment comment body
  * @param {string} id task id
-* */
+ * */
 export const addComment = (comment, id) => (dispatch, getState) => {
   dispatch({type: action.TASK_VIEW_REQUESTING})
   const body = JSON.stringify({comment})
