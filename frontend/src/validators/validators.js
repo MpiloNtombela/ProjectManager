8d1b8974df5e60201006695b3a5589b01d5e0b18
mpilo ntombela <m.ntombela458@gmail.com>
2021-03-02 17:34:11 +0200

    updated password validator doc description to use flow type

--- frontend/src/validators/validators.js
+++ frontend/src/validators/validators.js
@@ -1,22 +1,26 @@
-//standard password validator:: compares 2 pass(words)
-//: params(pass1, pass2, optional: minLength[default:8] ) ==> returns {valid(bool), message(string)}
+/**
+ * @description standard password validator:: compares 2 pass(words)
+ * @param {string} pass1 password1
+ * @param {string} pass2 password2
+ * @param {number} minLength minimum length of the password
+ * */
 const validatePasswords = (pass1, pass2, minLength = 8) => {
   if (pass1.trim().length < minLength || pass2.trim().length < minLength) {
     return {
       valid: false,
-      message: `password must be at least ${minLength} chars long(no spaces)`,
+      message: `password must be at least ${minLength} chars long(no spaces)`
     };
   } else if (pass1.trim() !== pass2.trim()) {
     return {
       valid: false,
-      message: "passwords don't match",
+      message: "passwords don't match"
     };
   } else {
     return {
       valid: true,
-      message: "ok",
+      message: "ok"
     };
   }
 };
 
-export { validatePasswords };
+export {validatePasswords};
