35426ae33beb94695f794f8d222ee0b6c513e1e5
mpilo ntombela <m.ntombela458@gmail.com>
2021-03-03 20:59:47 +0200

    changed Avatar component in favor of IconButton component

--- frontend/src/components/reuse/ReButtons.js
+++ frontend/src/components/reuse/ReButtons.js
@@ -2,10 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
-import Avatar from "@material-ui/core/Avatar";
-import ButtonBase from "@material-ui/core/ButtonBase";
 import {Close, Done} from "@material-ui/icons";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles(theme => ({
   cancelButton: {
@@ -38,32 +37,33 @@ const FormSubmitButton = React.memo(({children, disabled, size}) => (
   </Box>
 ));
 
-const SaveButton = ({type, onClick, disabled}) => {
+const SaveButton = ({type, onClick, disabled, size, className}) => {
   const classes = useStyles()
   return (
-    <Avatar
-      component={ButtonBase}
+    <IconButton
+      // component={ButtonBase}
       type={type}
       onClick={onClick}
-      className={classes.saveButton}
+      className={`${className} ${classes.saveButton}`}
       aria-label="save button"
-      disabled={disabled}>
+      disabled={disabled}
+      size={size ? size : 'small'}>
       <Done/>
-    </Avatar>
+    </IconButton>
   )
 }
-const CancelButton = ({type, onClick, disabled}) => {
+const CancelButton = ({type, onClick, disabled, size, className}) => {
   const classes = useStyles()
   return (
-    <Avatar
-      component={ButtonBase}
+    <IconButton
       type={type}
       onClick={onClick}
-      className={classes.cancelButton}
+      className={`${className} ${classes.cancelButton}`}
       aria-label='cancel button'
-      disabled={disabled}>
+      disabled={disabled}
+      size={size ? size : 'small'}>
       <Close/>
-    </Avatar>
+    </IconButton>
   )
 }
 FormSubmitButton.propTypes = {
@@ -74,13 +74,17 @@ FormSubmitButton.propTypes = {
 SaveButton.propTypes = {
   type: PropTypes.string,
   onClick: PropTypes.func,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  size: PropTypes.string,
+  className: PropTypes.string,
 };
 
 CancelButton.propTypes = {
   type: PropTypes.string,
   onClick: PropTypes.func,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  size: PropTypes.string,
+  className: PropTypes.string,
 };
 
 export {FormSubmitButton, SaveButton, CancelButton}
