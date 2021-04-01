a2d3bb47612631b943bbf4bee6eeff2210976ac0
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-04-01 15:37:51 +0200

    added functionality to add and remove subtasks

--- projects/urls.py
+++ projects/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from projects.apis.boards import ProjectBoardsListCreateAPI, BoardRetrieveUpdateDestroyAPI
 from projects.apis.projects import ProjectRetrieveAPI, ProjectMembersListAPI
 from projects.apis.tasks import (ProjectTaskListCreateAPI, BoardTaskCreateAPI, TaskRetrieveUpdateDestroyAPI,
-                                 TaskCommentListCreateAPI, TaskCommentDestroyAPI, AddRemoveTaskMemberAPI)
+                                 TaskCommentListCreateAPI, TaskCommentDestroyAPI, AddRemoveTaskMemberAPI, SubtaskListCreateAPI,
+                                 SubtaskRetrieveUpdateDestroyAPI)
 
 urlpatterns = [
     path('req/project/<str:pk>/', ProjectRetrieveAPI.as_view(), name='project-retrieve'),
@@ -12,8 +13,10 @@ urlpatterns = [
     path('req/board/<str:pk>/', BoardRetrieveUpdateDestroyAPI.as_view(), name='board-destroy'),
     path('req/board/<str:pk>/tasks/', BoardTaskCreateAPI.as_view(), name='board-tasks'),
     path('req/task/<str:pk>/', TaskRetrieveUpdateDestroyAPI.as_view(), name='task-retrieve-destroy'),
+    path('req/task/<str:pk>/subtasks/', SubtaskListCreateAPI.as_view(), name='subtasks'),
     path('req/task/<str:pk>/comments/', TaskCommentListCreateAPI.as_view(), name='task-comments'),
     path('req/task/<str:pk>/members/', AddRemoveTaskMemberAPI.as_view(), name='task-members'),
+    path('req/subtask/<str:pk>/', SubtaskRetrieveUpdateDestroyAPI.as_view(), name='subtask'),
     path('req/comment/<str:pk>/', TaskCommentDestroyAPI.as_view(), name='comment-destroy'),
 
     path('req/project/<str:pk>/users/', ProjectMembersListAPI.as_view(), name='project-members'),
