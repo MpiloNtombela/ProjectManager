9bf42d2a248ef48caa0e6ab0a03e35bb2074d981
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-04-14 19:32:59 +0200

    fixed bug that caused info alert not to render

--- frontend/src/components/feedback/SnackAlerts.js
+++ frontend/src/components/feedback/SnackAlerts.js
@@ -1,53 +1,32 @@
-import PropTypes from "prop-types";
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import Snackbar from "@material-ui/core/Snackbar";
 import Alert from "@material-ui/core/Alert";
 import {CLEAR_ALERTS} from "../../actions/types";
 
 const SnackAlerts = () => {
-  const [open, setOpen] = useState(false);
-  const [msg, setMsg] = useState("");
-  const [type, setType] = useState("");
   const dispatch = useDispatch ()
-  const alerts = useSelector(state => state.alerts)
+  const {alertType, alertMsg} = useSelector(state => state.alerts)
 
   const handleClose = () => {
-    setOpen(false);
-    setMsg("");
     dispatch({type: CLEAR_ALERTS})
   };
-  useEffect(() => {
-    if (alerts.statusCode) {
-      setMsg(alerts.alertMsg);
-      setType(`${alerts.alertType}`);
-      setOpen(true);
-    }
-  }, [alerts]);
-
+  
   return (
     <Snackbar
       anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-      open={open}
+      open={Boolean(alertMsg && alertType)}
       autoHideDuration={7000}
       onClose={handleClose}>
       <Alert
         onClose={handleClose}
-        severity={type ? type : "info"}
+        severity={alertType ? alertType : "info"}
         variant="filled"
         elevation={2}>
-        {msg}
+        {alertMsg}
       </Alert>
     </Snackbar>
   );
-};
-
-SnackAlerts.propTypes = {
-  alerts: PropTypes.shape({
-    alertMsg: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
-    alertType: PropTypes.string,
-    statusCode: PropTypes.number,
-  }),
 };
 
 export default SnackAlerts;
