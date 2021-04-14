94a9c01515830276b60aa45a4b44e18d2ef4bf79
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-04-14 19:35:16 +0200

    added support for reactQuery

--- frontend/src/components/projects/ProjectTemplate.js
+++ frontend/src/components/projects/ProjectTemplate.js
@@ -1,57 +1,117 @@
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
-import React, {Suspense, useEffect} from "react";
+import React, { Suspense } from "react";
 import ProjectPageSkeleton from "../skeletons/projects/ProjectPageSkeleton";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import {useDispatch} from "react-redux";
-import {useParams} from "react-router-dom";
-import {getProject} from "../../actions/projects/projects";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { tokenConfig } from "../common/axiosConfig";
+import { useQuery } from "react-query";
+import {
+  BOARDS_LOADED,
+  PROJECT_LOADED,
+  TASKS_LOADED,
+} from "../../actions/projectTypes";
 
+const ProjectView = React.lazy(() => import("./ProjectView"));
 
-const ProjectView = React.lazy(()=> import('./ProjectView'))
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    position: 'fixed',
+    position: "fixed",
     top: 0,
     bottom: 0,
     left: 0,
     right: 0,
-    // background: `linear-gradient(145deg, ${theme.palette.secondary.main} 30%, ${theme.palette.primary.main} 90%)`,
-    background: theme.palette.mode === 'light' ? '#eee' : theme.palette.background.default,
-    overflowY: 'auto',
-    minWidth: '100%',
-    paddingTop: '5rem',
-    scrollbarWidth: 'tiny',
-    '&::-webkit-scrollbar': {
-      width: '.25rem'
-    }
+    background:
+      theme.palette.mode === "light"
+        ? "#eee"
+        : theme.palette.background.default,
+    overflowY: "auto",
+    minWidth: "100%",
+    paddingTop: "3.75rem",
+    scrollbarWidth: "tiny",
+    "&::-webkit-scrollbar": {
+      width: ".25rem",
+    },
   },
   container: {
-    height: '100%',
-    padding: 0
-  }
-}))
+    height: "100%",
+    padding: 0,
+  },
+}));
+
+const ROOT_URL = `/api/projects/req/project`;
+const getProj = async (id, token) =>
+  await axios.get(`${ROOT_URL}/${id}/`, tokenConfig(token));
+
+const getBoards = async (id, token) =>
+  await axios.get(`${ROOT_URL}/${id}/boards/`, tokenConfig(token));
+
+const getTasks = async (id, token) =>
+  await axios.get(`${ROOT_URL}/${id}/tasks/`, tokenConfig(token));
 
 const ProjectTemplate = () => {
-  const classes = useStyles()
-  const dispatch = useDispatch()
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-  const {id} = useParams()
+  const { id } = useParams();
+  const { token } = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    dispatch(getProject(id))
-  }, [])
+  const {
+    isLoading: isProjectLoading,
+    isError: isProjectError,
+  } = useQuery(["project", id, token], () => getProj(id, token), {
+    onSuccess: ({ data }) => {
+      return dispatch({
+        type: PROJECT_LOADED,
+        payload: data,
+      });
+    },
+    // refetchInterval: 50000,
+    refetchOnWindowFocus: false,
+  });
+
+  const {
+    isLoading: isBoardsLoading,
+    isError: isBoardsError,
+  } = useQuery(["boards", id, token], () => getBoards(id, token), {
+    onSuccess: ({ data }) => {
+      return dispatch({
+        type: BOARDS_LOADED,
+        payload: data,
+      });
+    },
+    // refetchInterval: 10000,
+    refetchOnWindowFocus: false,
+  });
+
+  const {
+    isLoading: isTasksLoading,
+    isError: isTasksError,
+  } = useQuery(["tasks", id, token], () => getTasks(id, token), {
+    onSuccess: ({ data }) => {
+      return dispatch({
+        type: TASKS_LOADED,
+        payload: data,
+      });
+    },
+    // refetchInterval: 10000,
+    refetchOnWindowFocus: false,
+  });
+
+  const isLoading = Boolean(isProjectLoading || isBoardsLoading || isTasksLoading)
+  const isError = Boolean(isProjectError || isBoardsError || isTasksError)
 
   return (
     <Paper square elevation={0} className={classes.root}>
-      <Container maxWidth='xl' className={classes.container}>
-        <Suspense fallback={<ProjectPageSkeleton/>}>
-          <ProjectView/>
+      <Container maxWidth="xl" className={classes.container}>
+        <Suspense fallback={<ProjectPageSkeleton />}>
+          <ProjectView isLoading={isLoading} isError={isError}/>
         </Suspense>
       </Container>
     </Paper>
   );
-}
+};
 
 export default ProjectTemplate;
