ca758034625a4aa46c95bf72e663b2361cede89e
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-04-14 19:50:25 +0200

    added project drawer

---
+++ frontend/src/components/projects/ProjectDrawer.js
@@ -0,0 +1,45 @@
+// import React from 'react';
+//
+// const ProjectDrawer = () => {
+//   return (
+//     <Drawer
+//         className={classes.drawer}
+//         variant="persistent"
+//         anchor="right"
+//         open={open}
+//         classes={{
+//           paper: classes.drawerPaper,
+//         }}
+//       >
+//         <div className={classes.drawerHeader}>
+//           <IconButton onClick={handleDrawerClose}>
+//             {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+//           </IconButton>
+//         </div>
+//         <Divider />
+//         <List>
+//           {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+//             <ListItem button key={text}>
+//               <ListItemIcon>
+//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+//               </ListItemIcon>
+//               <ListItemText primary={text} />
+//             </ListItem>
+//           ))}
+//         </List>
+//         <Divider />
+//         <List>
+//           {['All mail', 'Trash', 'Spam'].map((text, index) => (
+//             <ListItem button key={text}>
+//               <ListItemIcon>
+//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+//               </ListItemIcon>
+//               <ListItemText primary={text} />
+//             </ListItem>
+//           ))}
+//         </List>
+//       </Drawer>
+//   );
+// };
+//
+// export default ProjectDrawer;
