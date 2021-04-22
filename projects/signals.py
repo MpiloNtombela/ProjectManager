16e8d05dc8c0be3a7a215d55677f05dfd91a7b92
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-04-22 11:46:47 +0200

    added signal to create init project setup

--- projects/signals.py
+++ projects/signals.py
@@ -1,19 +1,25 @@
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 
+from boards.models import Board
 from projects.models import Invitation, Project
+from tasks.models import Task
+
 
 @receiver(post_save, sender=Project)
-def create_project_invitation(sender, instance, created, **kwargs):
+def initial_project_setup(sender, instance, created, **kwargs):
     if created:
         Invitation.objects.create(project=instance)
-
-
-
-@receiver(post_save, sender=Project)
-def save_project_invitation(sender, instance, created, **kwargs):
-    if created:
-        print(f'-------------------{instance.members.all()}---------')
         instance.invitation.save()
         instance.invitation.gen_key()
-        instance.add_member(instance.creator)
+        Board.objects.bulk_create([
+            Board(name=_('To Do'), project=instance, creator=instance.creator),
+            Board(name=_('Currently Doing'), project=instance, creator=instance.creator),
+            Board(name=_('Completed'), project=instance, creator=instance.creator),
+        ])
+        task = Task.objects.create(creator=instance.creator,
+                                   board=Board.objects.filter(project=instance).first(),
+                                   name=_('A cool first task. Click to open TaskView'),
+                                   description=_("TaskView shows the progress of the task and everything related to it"))
+        task.save()
