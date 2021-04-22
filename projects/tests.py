7fc552930e2c3df13c9a5d0eef27cec87e2cf65b
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-04-22 11:16:30 +0200

    added coverage for testing

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
 
-# Create your tests here.
+
+
+class CreateProjectTests(APITestCase):
+    def test_create_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('rest_register')
+        data = {'first_name': 'mpilo',
+                'last_name' : 'xxx',
+                'username'  : 'Xman',
+                'email'     : 'mpilo@lol.com',
+                'password1' : 'Mpilo1234',
+                'password2' : 'Mpilo1234'
+                }
+        response = self.client.post(url, data, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+
+        self.assertEqual(User.objects.get().unsername, 'Xman')
