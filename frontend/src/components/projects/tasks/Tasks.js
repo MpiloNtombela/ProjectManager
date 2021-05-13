7ca965db5df1d7ca1142662bc25247a73ba1606a
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-05-13 19:09:18 +0200

    updated rendering of tasks

--- frontend/src/components/projects/tasks/Tasks.js
+++ frontend/src/components/projects/tasks/Tasks.js
@@ -1,49 +1,53 @@
-import React, {memo, useState} from 'react';
-import PropTypes from 'prop-types';
-import {PlaylistAdd} from "@material-ui/icons";
+import React, { memo, useState } from "react";
+import PropTypes from "prop-types";
+import { PlaylistAdd } from "@material-ui/icons";
 import Button from "@material-ui/core/Button";
 import NewTaskForm from "./forms/NewTaskForm";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import Task from "./Task";
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import TaskSkeleton from "../../skeletons/projects/TaskSkeleton";
 
-const Tasks = ({tasks, boardId}) => {
-  const [isNewTask, setIsNewTask] = useState(false)
-  const isAdding = useSelector(state => state.tasksState.isAdding)
+const Tasks = ({ tasks, boardId }) => {
+  const [isNewTask, setIsNewTask] = useState(false);
+  const isAdding = useSelector((state) => state.tasksState.isAdding);
 
   const handleClickAway = () => {
-    setIsNewTask(isAdding)
-  }
+    setIsNewTask(isAdding);
+  };
   const handleAddNewTaskClick = () => {
-    setIsNewTask(true)
+    setIsNewTask(true);
   };
 
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task}/>
+        <Task key={task.id} task={task} />
       ))}
-      {isAdding && isNewTask && <TaskSkeleton num={[1]}/>}
-      {isNewTask ?
+      {isAdding && isNewTask && <TaskSkeleton num={[1]} />}
+      {isNewTask ? (
         <ClickAwayListener onClickAway={handleClickAway}>
           <div>
-            <NewTaskForm setIsNewTask={setIsNewTask} boardId={boardId}/>
+            <NewTaskForm setIsNewTask={setIsNewTask} boardId={boardId} />
           </div>
         </ClickAwayListener>
-        : <Button
-          fullWidth color='secondary'
-          startIcon={<PlaylistAdd/>}
+      ) : (
+        <Button
+          fullWidth
+          color={"primary"}
+          startIcon={<PlaylistAdd />}
           onClick={handleAddNewTaskClick}
-          disabled={isAdding}>add new task</Button>
-      }
+          disabled={isAdding}>
+          add new task
+        </Button>
+      )}
     </>
-  )
-}
+  );
+};
 
 Tasks.propTypes = {
   tasks: PropTypes.array.isRequired,
-  boardId: PropTypes.string
-}
+  boardId: PropTypes.string,
+};
 
 export default memo(Tasks);
