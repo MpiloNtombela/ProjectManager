3addae29bdf89b4cd0a193587276affd8ae03c92
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-05-13 12:59:33 +0200

    added form proptypes

--- frontend/src/components/projects/tasks/forms/SubtaskForm.js
+++ frontend/src/components/projects/tasks/forms/SubtaskForm.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import RePopper from "../../../reuse/RePopper";
 import TextField from "@material-ui/core/TextField";
-import {CancelButton, SaveButton} from "../../../reuse/ReButtons";
+import { CancelButton, SaveButton } from "../../../reuse/ReButtons";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import {useDispatch} from "react-redux";
-import {addSubtask} from "../../../../actions/projects/tasks";
+import { useDispatch } from "react-redux";
+import { addSubtask } from "../../../../actions/projects/tasks";
 import createSnackAlert from "../../../../actions/snackAlerts";
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import PlaylistAddCheck from "@material-ui/icons/PlaylistAddCheck";
@@ -13,66 +13,70 @@ import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   form: {
-    width: '500px',
-    maxWidth: '100%',
-    display: 'flex'
+    width: "500px",
+    maxWidth: "100%",
+    display: "flex",
   },
-})
+});
 
+const SubtaskForm = ({ id }) => {
+  const classes = useStyles();
+  const [subtaskName, setSubtaskName] = useState("");
+  const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
 
-const SubtaskForm = ({id}) => {
-  const classes = useStyles()
-  const [subtaskName, setSubtaskName] = useState('')
-  const [open, setOpen] = useState(false)
-  const dispatch = useDispatch()
-  
-  const handleSubmit = e => {
-    e.preventDefault()
-    const name = subtaskName.trim()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = subtaskName.trim();
     if (name) {
-      dispatch(addSubtask(name, id))
-      setSubtaskName('')
+      dispatch(addSubtask(name, id));
+      setSubtaskName("");
     } else {
-      dispatch(createSnackAlert('subtask name is required', 400))
+      dispatch(createSnackAlert("subtask name is required", 400));
     }
-  }
+  };
   const handleOpen = () => {
-    setOpen(true)
-  }
+    setOpen(true);
+  };
   const handleClose = () => {
-    setOpen(false)
-  }
-  const handleChange = e => {
-    setSubtaskName(e.target.value)
-  }
+    setOpen(false);
+  };
+  const handleChange = (e) => {
+    setSubtaskName(e.target.value);
+  };
   return (
     <>
-      {open ?
+      {open ? (
         <ClickAwayListener onClickAway={handleClose}>
           <form onSubmit={handleSubmit} className={classes.form}>
             <TextField
-              aria-label={'subtask name'}
+              aria-label={"subtask name"}
               className={classes.textField}
               value={subtaskName}
               onChange={handleChange}
-              variant={'standard'}
+              variant={"standard"}
               placeholder={`subtask name`}
-              inputProps={{maxLength: 100, "aria-valuemax": 100}}
+              inputProps={{ maxLength: 100, "aria-valuemax": 100 }}
               autoFocus
-              fullWidth required/>
-            <div><SaveButton/></div>
+              fullWidth
+              required
+            />
+            <div>
+              <SaveButton />
+            </div>
           </form>
-        </ClickAwayListener> :
+        </ClickAwayListener>
+      ) : (
         <Button
           size="small"
-          startIcon={<PlaylistAddCheck/>}
+          startIcon={<PlaylistAddCheck />}
           disableElevation
           disableRipple
-          color='secondary'
+          color="primary"
           onClick={handleOpen}>
           add subtask
         </Button>
-      }
+      )}
     </>
   );
 };
