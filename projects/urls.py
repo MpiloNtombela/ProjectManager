0ef935219636909a0b7a8d1b2b2625ff8f60b4e2
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-05-13 13:04:56 +0200

    changed invite key structure, and added 'anyone' filed

--- projects/urls.py
+++ projects/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('req/projects/', ProjectListCreateAPI.as_view(), name='user-projects'),
     path('req/project/<str:pk>/', ProjectRetrieveAPI.as_view(), name='project-retrieve'),
     path('req/project/<str:pk>/invites', InvitationRetrieveUpdate.as_view(), name='project-invitation'),
-    path('req/project/doge', AcceptInviteAPI.as_view(), name='project-invitation-accept'),
+    path('req/project/invite', AcceptInviteAPI.as_view(), name='project-invitation-accept'),
     path('req/project/<str:pk>/members/', ProjectMembersListAPI.as_view(), name='project-members'),
 ]
