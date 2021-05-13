cb3bedcf19ef35c81c5cf40b42d614516743ef29
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-05-13 19:16:24 +0200

    replaced div with mui paper component for bg

--- frontend/src/components/reuse/RePopper.js
+++ frontend/src/components/reuse/RePopper.js
@@ -2,20 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import Popper from "@material-ui/core/Popper";
+import Paper from "@material-ui/core/Paper";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     zIndex: 1300,
     width: '250px',
-    maxWidth: '95vw'
+    maxWidth: '95vw',
   },
   paper: {
     margin: '.25rem .5rem',
     padding: theme.spacing(1),
-    backgroundColor: theme.palette.background.default,
-    boxShadow: '0 0 5px 3px rgba(0, 0, 0, .25)',
-    borderRadius: theme.shape.borderRadius
+    minHeight: 100
   }
 }));
 
@@ -30,9 +29,9 @@ const RePopper = ({anchorEl, setAnchorEl, children}) => {
   return (
     <Popper open={open} anchorEl={anchorEl} className={classes.root}>
       <ClickAwayListener onClickAway={handleClose}>
-        <div className={classes.paper}>
+        <Paper className={classes.paper} elevation={4}>
           {children}
-        </div>
+        </Paper>
       </ClickAwayListener>
     </Popper>
   );
