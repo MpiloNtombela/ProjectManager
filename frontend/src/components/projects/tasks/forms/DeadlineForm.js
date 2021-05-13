251363f5cd099f606f505749753a9f946e131b5e
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-05-13 12:59:33 +0200

    added form proptypes

--- frontend/src/components/projects/tasks/forms/DeadlineForm.js
+++ frontend/src/components/projects/tasks/forms/DeadlineForm.js
@@ -1,13 +1,10 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import DateTimePicker from "@material-ui/lab/DateTimePicker";
 import TextField from "@material-ui/core/TextField";
 import RePopper from "../../../reuse/RePopper";
 import {useDispatch} from "react-redux";
 import {updateTask} from "../../../../actions/projects/tasks";
-import MobileTimePicker from "@material-ui/lab/MobileTimePicker";
-import {MobileDateTimePicker} from "@material-ui/lab";
-import {getTimezoneValue} from "../../../../utils";
+import MobileDateTimePicker from "@material-ui/lab/MobileDateTimePicker";
 
 const DeadlineForm = ({setAnchorEl, anchorEl, deadline, id}) => {
   const [value, setValue] = useState(deadline);
@@ -25,14 +22,13 @@ const DeadlineForm = ({setAnchorEl, anchorEl, deadline, id}) => {
         inputFormat={'dd/MM/yyyy HH:mm'}
         ampm={false}
         disablePast
-        renderInput={(props) => <TextField value={value} variant={'standard'} {...props} />}
+        renderInput={(props) => <TextField fullWidth value={value} variant={'standard'} {...props} />}
         label="deadline"
         value={value}
         onChange={(newValue) => {
           setValue(newValue);
         }}
         onAccept={handleAccept}
-        showTodayButton={true}
       />
     </RePopper>
   );
