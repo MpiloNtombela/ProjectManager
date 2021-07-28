a9ffa9ae6ba9eda2174a470908a38b7ae89f3913
Mpilo Ntomblea <m.ntombela458@gmail.com>
2021-07-28 22:08:55 +0200

    emergency commit and push🚨🚨🚨

---
+++ base/exceptions.py
@@ -0,0 +1,14 @@
+from django.utils.translation import ugettext_lazy as _
+from rest_framework.exceptions import APIException
+from rest_framework.status import HTTP_400_BAD_REQUEST
+
+class BadRequest(APIException):
+    status_code = HTTP_400_BAD_REQUEST
+    default_detail = _('Bad request')
+    default_code = "bad_request"
+
+    def __init__(self, err_msg=None, code=None):
+        super().__init__(detail=err_msg, code=code)
+        if err_msg is None:
+            err_msg = self.default_detail
+        code = self.status_code
